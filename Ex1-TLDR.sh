31895cc4991c156fc538d1f4782890248958217d
utdsimmons <39208913+utdsimmons@users.noreply.github.com>
2020-07-25 20:13:20 -0500

    Add files via upload

--- Ex1-TLDR.sh
+++ Ex1-TLDR.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-CHEAT_DIR=~/tmp/cheat.$$
-cd $CHEAT_DIR
+TLDR_DIR=~/tmp/TLDR.$$
+cd $TLDR_DIR
 
 wget https://raw.githubusercontent.com/utdsimmons/pearc2020/master/ex1-cf.yml
 
-aws cloudformation deploy --template-file ex1-cf.yml --capabilities CAPABILITY_IAM --stack-name ex1-HALP --region us-east-1
+aws cloudformation deploy --template-file ex1-cf.yml --capabilities CAPABILITY_IAM --stack-name ex1-TLDR --region us-east-1
 
